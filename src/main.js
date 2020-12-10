7a511f9569c9221a8de0230d3dda9303e79bf412
gyf1150554303 <1150554303@qq.com>
2020-12-10 15:34:06 +0800

    router-view

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import Vant from "vant";
 import "vant/lib/index.css";
 //全局样式
 import "./styles/index.less";
+// 动态设置根元素字体大小
+import "amfe-flexible";
 
 Vue.use(Vant);
 Vue.config.productionTip = false;
