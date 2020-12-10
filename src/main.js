9f872dca7c27fe5b3bdb4c84d31409ca7fb5fe32
gyf1150554303 <1150554303@qq.com>
2020-12-10 15:24:08 +0800

    使用Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Vant from "vant";
+// Vant 的样式
+import "vant/lib/index.css";
 //全局样式
 import "./styles/index.less";
 
+Vue.use(Vant);
 Vue.config.productionTip = false;
 
 new Vue({
