a21516d8b6e7abae05462fa863fce2382ec115e6
gyf1150554303 <1150554303@qq.com>
2020-12-13 19:55:59 +0800

    下拉刷新

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="article-list">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      :error.sync="error"
-      error-text="请求失败,点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isRefreshLoading"
+      @refresh="onRefresh"
+      :success-text="refreshSuccessText"
+      :success-duration="1500"
     >
-      <van-cell
-        v-for="(article, index) in list"
-        :key="index"
-        :title="article.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in list"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -34,12 +41,14 @@ export default {
       finished: false, // 数据是否加载完成
       timestamp: null, //请求获取下一页数据的时间戳
       error: false, //控制列表失败的提示状态
+      isRefreshLoading: false, // 控制下拉刷新的 loading 状态
+      refreshSuccessText: "刷新成功", // 下拉刷新成功提示文本
     };
   },
   methods: {
     async onLoad() {
       try {
-        // 1.请求获取数据
+        // 1. 请求获取数据
         const { data } = await getArticles({
           channel_id: this.channel.id, // 频道 ID
           // 请求数据的页码，请求第 1 页数据，用当前最新时间戳
@@ -51,7 +60,6 @@ export default {
         /* if (Math.random() > 0.5) {
           JSON.parse('xxx')
         } */
-        // console.log(data);
         // 2. 把请求结果数据放到 list 数组中
         const { results } = data.data;
         this.list.push(...results);
@@ -59,7 +67,7 @@ export default {
         this.loading = false;
         // 4. 数据加载完毕后，要把 finished 设置为 true，不再触发加载更多了
         if (results.length) {
-          // 更新时间戳,用于获取下一页数据
+          // 更新时间戳，用于获取下一页数据
           this.timestamp = data.data.pre_timestamp;
         } else {
           // 没有数据了
@@ -68,8 +76,33 @@ export default {
       } catch (err) {
         // 展示错误提示状态
         this.error = true;
-        // 请求失败了,loading也需要关闭
+        // 请求失败了，loading 也需要关闭
         this.loading = false;
+      }
+    },
+    async onRefresh() {
+      try {
+        // 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1,
+        });
+        /* if (Math.random() > 0.5) {
+          JSON.parse('xxx')
+        } */
+        // 将数据追加到列表的顶部
+        const { results } = data.data;
+        this.list.unshift(...results);
+        // 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false;
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = `刷新成功，更新了${results.length}条数据`;
+      } catch (err) {
+        // 关闭下拉刷新的 loading 状态
+        this.isRefreshLoading = false;
+        // 更新下拉刷新成功提示的文本
+        this.refreshSuccessText = "刷新失败";
       }
     },
   },
