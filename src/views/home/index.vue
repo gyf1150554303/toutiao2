a56869f16442d3d9a591ccbd7d789e454d93350b
gyf1150554303 <1150554303@qq.com>
2020-12-13 15:18:50 +0800

    创建列表组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -18,8 +18,8 @@
         v-for="channel in channels"
         :key="channel.id"
         :title="channel.name"
-        >{{ channel.name }}的内容</van-tab
-      >
+        ><article-list :channel="channel"></article-list
+      ></van-tab>
       <div slot="nav-right" class="placeholder"></div>
       <div slot="nav-right" class="hamburger-btn">
         <i class="iconfont icongengduo"></i>
@@ -30,8 +30,12 @@
 
 <script>
 import { getUserChannels } from "@/api/user";
+import ArticleList from "./components/article-list";
 export default {
   name: "HomeIndex",
+  components: {
+    ArticleList,
+  },
   data() {
     return {
       active: 0,
