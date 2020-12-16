bf46e816cf33dfcec0b17e38f04205a8b4952f7d
gyf1150554303 <1150554303@qq.com>
2020-12-16 15:30:05 +0800

    子组件中监视父亲searchText的变化

--- src/views/search/components/search-suggestion.vue
+++ src/views/search/components/search-suggestion.vue
@@ -10,6 +10,22 @@
 <script>
 export default {
   name: "SearchSuggestion",
+  props: {
+    searchText: {
+      type: String,
+      required: true,
+    },
+  },
+  watch: {
+    searchText: {
+      // 当 searchText 发生变化的时候会调用 handler 函数（固定的）
+      handler(value) {
+        console.log(value);
+      },
+      // 第一次的时候也触发 handler
+      immediate: true,
+    },
+  },
 };
 </script>
 
