bedc00235b3cc95252a85bd3f0ccf67901c2a984
gyf1150554303 <1150554303@qq.com>
2020-12-19 21:00:10 +0800

    完成收藏逻辑

--- src/api/article.js
+++ src/api/article.js
@@ -1,19 +1,38 @@
 import request from "@/utils/request";
 
-//获取文章列表
-export const getArticles = (params) => {
+// 获取文章列表
+export const getArticles = params => {
   return request({
     method: "GET",
     url: "/app/v1_1/articles",
-    params,
+    params
   });
 };
 
 // 获取文章详情
-export const getArticleById = (articleId) => {
+export const getArticleById = articleId => {
   return request({
     method: "GET",
     // 注意这里是 v1_0!!!
-    url: `/app/v1_0/articles/${articleId}`,
+    url: `/app/v1_0/articles/${articleId}`
+  });
+};
+
+// 收藏文章
+export const addCollect = target => {
+  return request({
+    method: "POST",
+    url: "/app/v1_0/article/collections",
+    data: {
+      target
+    }
+  });
+};
+
+// 取消收藏文章
+export const deleteCollect = target => {
+  return request({
+    method: "DELETE",
+    url: `/app/v1_0/article/collections/${target}`
   });
 };
