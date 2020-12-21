91349e5b91c285a1c18e5974b55d612eb2fdb01d
gyf1150554303 <1150554303@qq.com>
2020-12-21 21:32:34 +0800

    页面布局

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    x
+  <div class="user-profile">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      class="page-nav-bar"
+      title="个人信息"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <!-- /导航栏 -->
+    <!-- 个人信息 -->
+    <van-cell title="头像" is-link>
+      <van-image
+        class="avatar"
+        fit="cover"
+        round
+        src="https://img.yzcdn.cn/vant/cat.jpeg"
+      />
+    </van-cell>
+    <van-cell title="昵称" value="内容" is-link />
+    <van-cell title="性别" value="内容" is-link />
+    <van-cell title="生日" value="内容" is-link />
+    <!-- /个人信息 -->
   </div>
 </template>
 
@@ -14,4 +34,15 @@ export default {
 };
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus"></style>
+<style lang="less" scoped>
+.user-profile {
+  .avatar {
+    width: 60px;
+    height: 60px;
+  }
+
+  .van-popup {
+    background-color: #f5f7f9;
+  }
+}
+</style>
