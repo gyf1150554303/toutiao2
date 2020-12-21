2c661226866f0069fcd400132ff71ed080c134f5
gyf1150554303 <1150554303@qq.com>
2020-12-21 21:40:22 +0800

    创建组件

---
+++ src/views/user-profile/components/update-name.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="update-name">
+    更新昵称
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UpdateName",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped></style>
