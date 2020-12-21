3f12c28d5903e48a24508cd70e389fc159ddb578
gyf1150554303 <1150554303@qq.com>
2020-12-21 15:25:18 +0800

    处理头部及当前评论项

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="comment-reply">
-    评论回复
+    <van-nav-bar
+      :title="
+        comment.reply_count > 0 ? `${comment.reply_count}条回复` : '暂无回复'
+      "
+    >
+      <van-icon slot="left" name="cross" @click="$emit('close')" />
+    </van-nav-bar>
+    <!-- 当前评论项 -->
+    <comment-item :comment="comment" />
+    <!-- /当前评论项 -->
   </div>
 </template>
 
 <script>
+import CommentItem from "./comment-item";
 export default {
   name: "CommentReply",
+  components: {
+    CommentItem
+  },
   props: {
     comment: {
       type: Object,
